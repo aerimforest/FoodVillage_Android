07cac511e60c8567d43b16bddcfef91798430d57
aerimforest <aerimforest98@gmail.com>
2021-11-25 05:25:11 +0900

    [refactor] 코드 리팩토링

--- app/src/main/java/com/example/foodvillage/storeList/StoreAdapter.kt
+++ app/src/main/java/com/example/foodvillage/storeList/StoreAdapter.kt
@@ -8,32 +8,32 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.foodvillage.R
 
-class StoreAdapter(var storeList : ArrayList<StoreInfo> ) : RecyclerView.Adapter<StoreAdapter.CustomViewHolder>() {
+class StoreAdapter(var storeList: ArrayList<StoreInfo>) :
+    RecyclerView.Adapter<StoreAdapter.CustomViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.activity_store_list_item, parent, false)
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.activity_store_list_item, parent, false)
         return CustomViewHolder(view).apply {
-            itemView.setOnClickListener{
+            itemView.setOnClickListener {
                 val curPos: Int = adapterPosition
-                val store: StoreInfo = storeList.get(curPos)
+                val store: StoreInfo = storeList[curPos]
             }
-
         }
-
     }
 
     // view를 실제 어댑터에 연결
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
-        holder.storeImage.setImageResource(storeList.get(position).storeImage)
-        holder.name.text = storeList.get(position).storeName
-        holder.dist.text = storeList.get(position).distance
+        holder.storeImage.setImageResource(storeList[position].storeImage)
+        holder.name.text = storeList[position].storeName
+        holder.dist.text = storeList[position].distance
         //holder.review.text = "리뷰 "
-        holder.reviewNum.text = storeList.get(position).reviewTotal
+        holder.reviewNum.text = storeList[position].reviewTotal
         //holder.product.text = "상품 수"
-        holder.productNum.text = storeList.get(position).prodNumTotal
-        holder.category.text = storeList.get(position).categories.toString()
+        holder.productNum.text = storeList[position].prodNumTotal
+        holder.category.text = storeList[position].categories.toString()
         //holder.sale.text = "최대 할인율"
-        holder.salePercent.text = storeList.get(position).salePercentMax
+        holder.salePercent.text = storeList[position].salePercentMax
     }
 
     override fun getItemCount(): Int {
@@ -41,19 +41,23 @@ class StoreAdapter(var storeList : ArrayList<StoreInfo> ) : RecyclerView.Adapter
     }
 
     class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val storeImage = itemView.findViewById<ImageView>(R.id.iv_storeImage)
-        val name = itemView.findViewById<TextView>(R.id.tv_storeName) // 가게이름
-        val dist = itemView.findViewById<TextView>(R.id.tv_distance) // 거리
+        val storeImage: ImageView = itemView.findViewById(R.id.iv_storeImage)
+        val name: TextView = itemView.findViewById(R.id.tv_storeName) // 가게이름
+        val dist: TextView = itemView.findViewById(R.id.tv_distance) // 거리
+
         //val review = itemView.findViewById<TextView>(R.id.tv_review) // 리뷰
-        val reviewNum = itemView.findViewById<TextView>(R.id.tv_review_num) // 리뷰 갯수
+        val reviewNum: TextView = itemView.findViewById(R.id.tv_review_num) // 리뷰 갯수
+
         //val product = itemView.findViewById<TextView>(R.id.tv_product) // 상품
-        val productNum = itemView.findViewById<TextView>(R.id.tv_product_num) // 상품 갯수
-        val category = itemView.findViewById<TextView>(R.id.tv_category) // 카테고리
+        val productNum: TextView = itemView.findViewById(R.id.tv_product_num) // 상품 갯수
+        val category: TextView = itemView.findViewById(R.id.tv_category) // 카테고리
+
         //val sale = itemView.findViewById<TextView>(R.id.tv_sale) // 최대 할인률
-        val salePercent = itemView.findViewById<TextView>(R.id.tv_sale_percetage) //
+        val salePercent: TextView = itemView.findViewById(R.id.tv_sale_percetage)
     }
-    fun datasetChanged(storeList: ArrayList<StoreInfo>){
-        this.storeList=storeList
+
+    fun datasetChanged(storeList: ArrayList<StoreInfo>) {
+        this.storeList = storeList
         notifyDataSetChanged()
     }
 }
