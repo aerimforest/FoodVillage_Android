8d5ba27c6926ddedd897542ccd723e6989e1020f
aerimforest <aerimforest98@gmail.com>
2021-11-26 01:35:52 +0900

    [feat] 가게정보 & 리뷰 레이아웃 구성

--- app/src/main/java/com/example/foodvillage/storeInfo/ui/StoreInfoActivity.kt
+++ app/src/main/java/com/example/foodvillage/storeInfo/ui/StoreInfoActivity.kt
@@ -2,16 +2,17 @@ package com.example.foodvillage.storeInfo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.foodvillage.R
 import com.example.foodvillage.databinding.ActivityStoreInfoBinding
 import com.example.foodvillage.schema.Product
+import com.example.foodvillage.schema.Store
 import com.example.foodvillage.storeInfo.adapter.StoreCategory
 import com.example.foodvillage.storeInfo.adapter.StoreInfoCategoryAdapter
 import com.example.foodvillage.storeInfo.adapter.StoreInfoProductAdapter
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 
 class StoreInfoActivity : AppCompatActivity() {
 
@@ -34,6 +35,9 @@ class StoreInfoActivity : AppCompatActivity() {
     private var databaseReference: DatabaseReference = firebaseDatabase.reference
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
+    //val storeName = intent.getStringExtra("storeName")
+    val storeName = "나연마트1"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityStoreInfoBinding.inflate(layoutInflater)
@@ -53,13 +57,75 @@ class StoreInfoActivity : AppCompatActivity() {
             LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         binding.rcvStoreInfoProduct.setHasFixedSize(true)
 
-        val storeName = intent.getStringExtra("storeName")
-
         // Todo: storeName으로 Product 테이블 접근해서 productList 업데이트
 
-        if (storeName != null) {
-            iconHeartClickEvent(storeName)
+        setStoreInfo()
+
+//        if (storeName != null) {
+//            iconHeartClickEvent(storeName)
+//        }
+
+        binding.tvProductInfo.setOnClickListener {
+            productInfoClickEvent()
         }
+
+        binding.tvStoreInfo.setOnClickListener {
+            storeInfoClickEvent()
+        }
+
+        binding.tvReview.setOnClickListener {
+            tvReviewClickEvent()
+        }
+    }
+
+    private fun setStoreInfo() {
+        val databaseStoreReference: DatabaseReference =
+            firebaseDatabase.getReference(storeName)
+
+        databaseStoreReference.addValueEventListener(object :
+            ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                for (postSnapshot in dataSnapshot.children) {
+                    val item = postSnapshot.getValue(Store::class.java)
+                    if (item != null) {
+                        if (item.storeName == storeName) {
+                            binding.tvStoreInfoName.text = item.storeName
+                            binding.tvStoreInfoPhone.text = item.phoneNumber
+                            binding.tvStoreInfoTime2.text = item.time
+                            binding.tvStoreInfoTime.text = item.time
+                            binding.tvStoreInfoBreak.text = item.dayOff
+                            binding.tvStoreInfoLocation.text = item.address
+
+                            // 거리
+                            val databaseDistanceReference: DatabaseReference =
+                                firebaseDatabase.getReference("stores/$storeName/distance/${auth.uid}")
+
+                            databaseDistanceReference.addValueEventListener(object :
+                                ValueEventListener {
+                                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                                    binding.tvStoreInfoDistance.text = dataSnapshot.value.toString()
+                                }
+
+                                override fun onCancelled(error: DatabaseError) {
+                                }
+                            })
+
+                            // drawable 파일에서 이미지 검색 후 적용
+                            val id = resources.getIdentifier(
+                                item.storeImg.toString(),
+                                "drawable",
+                                packageName
+                            )
+                            binding.imvStoreInfoImg.setImageResource(id)
+                        }
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
     }
 
     private fun iconHeartClickEvent(storeName: String) {
@@ -68,5 +134,25 @@ class StoreInfoActivity : AppCompatActivity() {
             databaseReference = firebaseDatabase.getReference("stores/$storeName")
             databaseReference.child("dibPeople").push().setValue(auth.uid)
         }
+    }
+
+    private fun productInfoClickEvent() {
+        binding.svStoreInfoProduct.visibility = View.VISIBLE
+        binding.llyStoreInfoStore.visibility = View.INVISIBLE
+        binding.llyStoreInfoReview.visibility = View.INVISIBLE
+    }
+
+    private fun storeInfoClickEvent() {
+        binding.svStoreInfoProduct.visibility = View.INVISIBLE
+        binding.llyStoreInfoStore.visibility = View.VISIBLE
+        binding.llyStoreInfoReview.visibility = View.INVISIBLE
+
+        // Todo: 길찾기 버튼 클릭 이벤트
+    }
+
+    private fun tvReviewClickEvent() {
+        binding.svStoreInfoProduct.visibility = View.INVISIBLE
+        binding.llyStoreInfoStore.visibility = View.INVISIBLE
+        binding.llyStoreInfoReview.visibility = View.VISIBLE
     }
 }
