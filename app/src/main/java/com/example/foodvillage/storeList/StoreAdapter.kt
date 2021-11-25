be223889425b7cf45e89bccd58b5b54ef485db2f
nkeum <rmaskdus0208@gmail.com>
2021-11-26 03:35:21 +0900

    [feat] 가게 리스트 뷰 이미지 추가 - 완료 - image variation 필요하긴 함

--- app/src/main/java/com/example/foodvillage/storeList/StoreAdapter.kt
+++ app/src/main/java/com/example/foodvillage/storeList/StoreAdapter.kt
@@ -2,6 +2,8 @@ package com.example.foodvillage.storeList
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Paint
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,15 +12,25 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.foodvillage.R
 import com.example.foodvillage.storeInfo.ui.StoreInfoActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.item_today_sale.view.*
 
-class StoreAdapter(var storeList: ArrayList<StoreInfo>) :
+class StoreAdapter(var storeList: ArrayList<StoreInfo>, private val mContext:Context) :
     RecyclerView.Adapter<StoreAdapter.CustomViewHolder>() {
 
-    private val mContext: Context? = null
+    private val firebaseDatabase: FirebaseDatabase = FirebaseDatabase.getInstance()
+    private var databaseReference: DatabaseReference = firebaseDatabase.reference
+    private var databaseStoreReference: DatabaseReference = firebaseDatabase.reference
+
+    var uid = FirebaseAuth.getInstance().uid
+    var DbRefUser = firebaseDatabase.getReference("users/" + uid)
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.activity_store_list_item, parent, false)
+
         return CustomViewHolder(view).apply {
             itemView.setOnClickListener {
                 val curPos: Int = adapterPosition
@@ -29,7 +41,7 @@ class StoreAdapter(var storeList: ArrayList<StoreInfo>) :
 
     // view를 실제 어댑터에 연결
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
-        holder.storeImage.setImageResource(storeList[position].storeImage)
+        //holder.storeImage.setImageResource(storeList[position].storeImage)
         holder.name.text = storeList[position].storeName
         holder.dist.text = storeList[position].distance
         //holder.review.text = "리뷰 "
@@ -39,6 +51,39 @@ class StoreAdapter(var storeList: ArrayList<StoreInfo>) :
         holder.category.text = storeList[position].categories.toString()
         //holder.sale.text = "최대 할인율"
         holder.salePercent.text = storeList[position].salePercentMax
+
+
+        Log.d("리소스", storeList[position].storeImg.toString())
+        // drawable 파일에서 이미지 검색 후 적용
+        val id = mContext!!.resources.getIdentifier(
+            storeList[position].storeImg.toString(),
+            "drawable",
+            mContext!!.packageName
+        )
+        holder.storeImage.setImageResource(id)
+//
+//        val auth: FirebaseAuth = FirebaseAuth.getInstance()
+//        val databaseDistanceReference: DatabaseReference =
+//            firebaseDatabase.getReference("stores/${storeList[position].storeName}/distance/${auth.uid}")
+
+//        databaseDistanceReference.addValueEventListener(object :
+//            ValueEventListener {
+//            override fun onDataChange(dataSnapshot: DataSnapshot) {
+//                holder.tv_distance.text = dataSnapshot.value.toString()
+//            }
+//
+//            override fun onCancelled(error: DatabaseError) {
+//            }
+//        })
+
+//        holder.tv_fixed_price.apply {
+//            paintFlags = paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+//        }
+//        holder.tv_fixed_price_won.apply {
+//            paintFlags = paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+//        }
+
+
 
         holder.itemView.setOnClickListener(View.OnClickListener {
             val intent = Intent(mContext, StoreInfoActivity::class.java)
